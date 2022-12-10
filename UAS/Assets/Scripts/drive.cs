838e1f3e240e5b10c7f494479d78d937e6b394af
StevenGeraldi <steven.geraldi@student.umn.ac.id>
2022-12-10 23:51:29 +0700

    update
    update

---
+++ UAS/Assets/Scripts/drive.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drive : MonoBehaviour
+{
+    public float speed = 2.5f;
+    public float rotationSpeed = 100.0f;
+    Animator anim;
+
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.fixedDeltaTime;
+        rotation *= Time.fixedDeltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if (translation > 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isJumping", false);
+            anim.SetFloat("botSpeed", 1);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+                anim.SetFloat("botSpeed", 1);
+            }
+        }
+        else if (translation < 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            anim.SetBool("isRunning", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isJumping", false);
+            anim.SetFloat("botSpeed", -1);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isWalking", false);
+                anim.SetBool("isRunning", true);
+                anim.SetFloat("botSpeed", -1);
+            }
+        }
+        else if (translation == 0 && Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("isJumping", true);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isRunning", false);
+            anim.SetFloat("botSpeed", translation);
+        }
+
+        else if (translation < 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isJumping", false);
+            anim.SetFloat("botSpeed", -1);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+                anim.SetFloat("botSpeed", -1);
+            }
+        }
+        else if (translation > 0)
+        {
+            anim.SetBool("isWalking", true);
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isJumping", false);
+            anim.SetFloat("botSpeed", 1);
+            if (Input.GetKey(KeyCode.Space))
+            {
+                anim.SetBool("isJumping", true);
+                anim.SetBool("isWalking", true);
+                anim.SetBool("isRunning", false);
+                anim.SetFloat("botSpeed", 1);
+            }
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+            anim.SetBool("isWalking", false);
+            anim.SetBool("isJumping", false);
+            anim.SetFloat("botSpeed", 0);
+        }
+
+        
+
+    }
+
+}
